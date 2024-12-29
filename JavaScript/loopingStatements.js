f8df8e0f96cee9540b985b917f282e875a2191a5
Prasanth K S <ksprasanthksp10@gmail.com>
2024-12-29 14:14:11 +0530

    Final Commit for JavaScript holds callback,promise,async/await,es6

--- JavaScript/loopingStatements.js
+++ JavaScript/loopingStatements.js
@@ -1,5 +1,5 @@
 array = [10,20,30,[40.5,50.6],"a","KEC",true]
-console.log(array);
+// console.log(array);
 
 // 1.for...in Loop
 // for (var a in array){
@@ -8,6 +8,25 @@ console.log(array);
 
 // 2. for...of loop
 
-for(var b of array){
-    console.log(b);
+// for(var b of array){
+//     console.log(b);
+// }
+
+// 3. foreach
+// Example 1
+
+// array.forEach((index,element)=>{
+//     console.log("Index is", index, "and it's Element is",element);
+// })
+
+// Example 2
+objType1 = {
+    "firstName": "Jai",
+    "lastName" : "Kumar",
+    "age" : 20
+}
+console.log(objType1);
+for (key in objType1){
+    value = objType1[key]
+    console.log(key,value)
 }
